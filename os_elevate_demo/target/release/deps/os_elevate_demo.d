903fa91ca88337e552a9df45090c2ea719aaebfb
Yug Rokadia <yug.rokadia75@nmims.in>
2025-09-25 18:27:17 +0530

    Delete os_elevate_demo directory

--- os_elevate_demo/target/release/deps/os_elevate_demo.d
+++
@@ -1,5 +0,0 @@
-E:\SIH\os_elevate_demo\target\release\deps\os_elevate_demo.d: src\main.rs
-
-E:\SIH\os_elevate_demo\target\release\deps\os_elevate_demo.exe: src\main.rs
-
-src\main.rs:
--- os_elevate_demo/target/release/os_elevate_demo.d
+++
@@ -1,1 +0,0 @@
-E:\SIH\os_elevate_demo\target\release\os_elevate_demo.exe: E:\SIH\os_elevate_demo\src\main.rs
